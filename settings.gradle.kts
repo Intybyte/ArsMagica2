aac09523cda30e4432818631e5993885cbfb053d
Intybyte <vaan.testwork@gmail.com>
2025-09-10 12:48:09 +0200

    BREATH AGAIN

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+rootProject.name = "AM2"
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jenkins.usrv.eu:8081/nexus/content/repositories/releases/")
+        maven {
+            name = "OvermindDL1 Maven"
+            url = uri("https://gregtech.overminddl1.com/")
+            metadataSources {
+                mavenPom()
+                gradleMetadata()
+                artifact()
+            }
+        }
+
+        maven {
+            name = "GTNH Maven"
+            url = uri("https://nexus.gtnewhorizons.com/repository/public/")
+        }
+
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.architectury.dev/")
+        maven("https://files.minecraftforge.net/maven/")
+    }
+}
